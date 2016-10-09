b34d5bda46ca3876cfae1460919ab888237aba38
Sebastian Puskeiler <s.puskeiler@mytaxi.com>
2016-10-10 01:12:54 +0200

    * added icon

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ alfredNotifier();
 
 var config = require('./config.json');
 var options = {
-    method: 'GET', 
+    method: 'GET',
     headers: {
         'Content-Type': 'application/json',
-	'Authorization': 'Basic ' + config.auth
+        'Authorization': 'Basic ' + config.auth
     }
 };
 
 alfy.fetch(config.url + '/rest/api/latest/plan?max-result=300', options).then(data => {
-  const list = data['plans']['plan'];
-  const items = alfy
+    const list = data['plans']['plan'];
+    const items = alfy
         .matches(alfy.input, list, 'name')
         .map(x => ({
             title: x.name,
             subtitle: x.shortName,
             arg: 'https://bamboo.mytaxi.com/browse/' + x.key
         }));
-  
-  alfy.output(items);
+
+    alfy.output(items);
 });
