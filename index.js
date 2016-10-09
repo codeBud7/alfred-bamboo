f1fba5c9380d5c830a92fec6932ed8b39c3aee88
Sebastian Puskeiler <s.puskeiler@mytaxi.com>
2016-10-09 22:47:31 +0200

    make bamboo settings configurable

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const alfredNotifier = require('alfred-notifier');
 
 alfredNotifier();
 
+var config = require('./config.json');
 var options = {
     method: 'GET', 
     headers: {
         'Content-Type': 'application/json',
-	'Authorization': 'Basic {user:password}'
+	'Authorization': 'Basic ' + config.auth
     }
 };
 
-alfy.fetch('bamboo.api.com/rest/api/latest/plan?max-result=300', options).then(data => {
+alfy.fetch(config.url + '/rest/api/latest/plan?max-result=300', options).then(data => {
   const list = data['plans']['plan'];
   const items = alfy
         .matches(alfy.input, list, 'name')
